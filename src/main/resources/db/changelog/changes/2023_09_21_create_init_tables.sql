7755ee51bf64384580f132da90bea9fd8a8199bb
Takhirjan <takhirjon22!@mail.ru>
2023-10-13 21:35:02 +0600

    Final Migration

--- src/main/resources/db/changelog/changes/2023_09_21_create_init_tables.sql
+++ src/main/resources/db/changelog/changes/2023_09_21_create_init_tables.sql
@@ -1,38 +1,43 @@
-CREATE TABLE t_permission(
-    id bigint auto_increment,
-    role varchar(255),
-    primary key (id)
-);
-CREATE TABLE t_apartaments(
-                              id bigint auto_increment,
-                              title varchar(255),
-                              primary key (id)
+
+CREATE TABLE t_permission (
+  id bigint auto_increment,
+  role varchar(255),
+  primary key (id)
 );
 
-CREATE TABLE t_users(
-    id bigint auto_increment,
-    email varchar(255),
-    full_name varchar(255),
-    password varchar(255),
-    primary key (id)
+CREATE TABLE t_users (
+                         id bigint auto_increment,
+                         email varchar(255),
+                         full_name varchar(255),
+                         password varchar(255),
+                         primary key (id)
 );
 
-CREATE TABLE t_users_permissions(
-    user_id bigint,
-    permission_id bigint
+CREATE TABLE t_users_permissions (
+ users_id bigint,
+ permissions_id bigint
 );
 
+-- Add foreign key constraint for t_users_permissions -> t_users
 ALTER TABLE t_users_permissions
     ADD CONSTRAINT FK_t_users_permissions_user_id
-        FOREIGN KEY (user_id)
+        FOREIGN KEY (users_id)
             REFERENCES t_users(id)
                 ON DELETE CASCADE;
 
 ALTER TABLE t_users_permissions
     ADD CONSTRAINT FK_t_users_permissions_permission_id
-        FOREIGN KEY (permission_id)
+        FOREIGN KEY (permissions_id)
             REFERENCES t_permission(id)
-                ON DELETE CASCADE;
+            ON DELETE CASCADE;
+
+
+
+CREATE TABLE t_apartaments(
+                              id bigint auto_increment,
+                              title varchar(255),
+                              primary key (id)
+);
 
 
 CREATE TABLE t_service(
@@ -60,6 +65,7 @@ CREATE TABLE t_hotels(
  city varchar(255),
  address varchar(255),
  primary key (id)
+
 );
 
 CREATE TABLE t_comments(
